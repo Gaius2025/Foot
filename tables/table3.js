b377ac0814e4e2d4113ba0bba9e4984356639484
Gaius2025 <bgparissportifs@gmail.com>
2025-10-11 19:48:26 +0200

    Update table3.json

--- tables/table3.js
+++ tables/table3.js
@@ -1,1 +1,94 @@
+{
+  "Czech Republic": {
+    "leagues": [
+      {
+        "name": "Czech First League",
+        "id": 172
+      },
+      {
+        "name": "FNL",
+        "id": 205
+      },
+      {
+        "name": "CFL",
+        "id": 9387
+      },
+      {
+        "name": "MSFL",
+        "id": 9386
+      },
+      {
+        "name": "Czech Cup",
+        "id": 282
+      },
+      {
+        "name": "Super Cup",
+        "id": 798
+      }
+    ]
+  },
+  "Switzerland": {
+    "leagues": [
+      {
+        "name": "Swiss Super League",
+        "id": 215
+      },
+      {
+        "name": "Challenge League",
+        "id": 216
+      },
+      {
+        "name": "Promotion League",
+        "id": 1101
+      },
+      {
+        "name": "Schweizer Cup",
+        "id": 399
+      }
+    ]
+  },
+  "Sweden": {
+    "leagues": [
+      {
+        "name": "Allsvenskan",
+        "id": 40
+      },
+      {
+        "name": "Superettan",
+        "id": 46
+      },
+      {
+        "name": "Ettan, Norra",
+        "id": 67
+      },
+      {
+        "name": "Ettan, Södra",
+        "id": 68
+      },
+      {
+        "name": "Division 2, Promotion Round",
+        "id": 10641
+      },
+      {
+        "name": "Division 2, Norrland",
+        "id": 71
+      },
+      {
+        "name": "Division 2, Norra Svealand",
+        "id": 70
+      },
+      {
+        "name": "Division 2, Södra Svealand",
+        "id": 74
+      },
+      {
+        "name": "Division 2, Norra Götaland",
+        "id": 868
+      },
+      {
+        "name": "Division 2, Ostra Götaland",
+        "id": 395
+      },
+      {
+        "name": "Division 2, Västra Göt
 
