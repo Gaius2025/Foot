a26efe1ea0c9d12b4746be80f0f9e8d5560be0c9
Gaius2025 <bgparissportifs@gmail.com>
2025-10-11 19:43:14 +0200

    Update table2.js

--- tables/table2.js
+++ tables/table2.js
@@ -1,1 +1,102 @@
-
+{
+  "South Africa": {
+    "leagues": [
+      {
+        "name": "South African Premier Division",
+        "id": 358
+      },
+      {
+        "name": "Motsepe Foundation Championship",
+        "id": 1906
+      },
+      {
+        "name": "SAFA Women's League",
+        "id": 25484
+      },
+      {
+        "name": "South Africa Diski Challenge",
+        "id": 23696
+      }
+    ]
+  },
+  "Bulgaria": {
+    "leagues": [
+      {
+        "name": "Parva Liga",
+        "id": 247
+      },
+      {
+        "name": "Vtora Liga",
+        "id": 1135
+      },
+      {
+        "name": "Women's Super League",
+        "id": 24121
+      }
+    ]
+  },
+  "Bolivia": {
+    "leagues": [
+      {
+        "name": "División Profesional",
+        "id": 16736
+      },
+      {
+        "name": "Liga Femenina",
+        "id": 22751
+      }
+    ]
+  },
+  "Algeria": {
+    "leagues": [
+      {
+        "name": "Algerian Ligue 1",
+        "id": 841
+      },
+      {
+        "name": "Algerian Ligue 2",
+        "id": 9459
+      }
+    ]
+  },
+  "South Korea": {
+    "leagues": [
+      {
+        "name": "K League 1",
+        "id": 410
+      },
+      {
+        "name": "K League 2",
+        "id": 777
+      },
+      {
+        "name": "K3 League",
+        "id": 10268
+      },
+      {
+        "name": "K4 League",
+        "id": 25158
+      }
+    ]
+  },
+  "Scotland": {
+    "leagues": [
+      {
+        "name": "Scottish Premiership",
+        "id": 36
+      },
+      {
+        "name": "Scottish Championship",
+        "id": 206
+      },
+      {
+        "name": "Scottish League One",
+        "id": 207
+      },
+      {
+        "name": "Scottish League Two",
+        "id": 209
+      }
+    ]
+  }
+}
