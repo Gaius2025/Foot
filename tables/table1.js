d46adcf57ef0f9d01c69c6ba9715be8fd101cdd2
Gaius2025 <bgparissportifs@gmail.com>
2025-10-11 19:32:17 +0200

    Update table1.js

--- tables/table1.js
+++ tables/table1.js
@@ -1,1 +1,90 @@
-
+{
+  "Uzbekistan": {
+    "leagues": [
+      {
+        "name": "Uzbekistan Super League",
+        "id": 772
+      },
+      {
+        "name": "Uzbekistan Pro Legue",
+        "id": 18559
+      }
+    ]
+  },
+  "Israel": {
+    "leagues": [
+      {
+        "name": "Israeli Premier League",
+        "id": 266
+      },
+      {
+        "name": "Israel National League",
+        "id": 727
+      },
+      {
+        "name": "Israeli Women's Football League",
+        "id": 14586
+      },
+      {
+        "name": "Liga Alef North",
+        "id": 23265
+      },
+      {
+        "name": "Liga Alef South",
+        "id": 23266
+      }
+    ]
+  },
+  "Ireland": {
+    "leagues": [
+      {
+        "name": "Premier Division",
+        "id": 192
+      },
+      {
+        "name": "Ireland 1st Division",
+        "id": 193
+      },
+      {
+        "name": "Ireland Women's Premier Division",
+        "id": 20065
+      },
+      {
+        "name": "Beamish Stout Munster Senior Premier Division",
+        "id": 22758
+      }
+    ]
+  },
+  "Northern Ireland": {
+    "leagues": [
+      {
+        "name": "NIFL Premiership",
+        "id": 200
+      },
+      {
+        "name": "NIFL Championship",
+        "id": 701
+      },
+      {
+        "name": "Northern Ireland Women's Premiership",
+        "id": 26136
+      }
+    ]
+  },
+  "Armenia": {
+    "leagues": [
+      {
+        "name": "Armenian Premier League",
+        "id": 671
+      },
+      {
+        "name": "Armenian 1st League",
+        "id": 672
+      },
+      {
+        "name": "Women’s Highest League",
+        "id": 24055
+      }
+    ]
+  }
+}
